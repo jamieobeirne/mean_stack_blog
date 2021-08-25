61965d4b0f8edb0e52f5a2e7a8b82ded01c1592a
Jamie OBeirne <jamieobeirne123@gmail.com>
2021-08-25 16:21:56 +0200

    Aug 25 420pm

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 
 const postsRoutes = require("./routes/posts");
 
+
 const app = express();
 
 mongoose
@@ -23,7 +24,7 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("backend/images")));
+app.use("/images", express.static(path.join("images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -37,6 +38,8 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+
 
 app.use("/api/posts", postsRoutes);
 
